b59db5b4301438877f1d74d3e405017e635e1010
JamieTran <32204301+JamieTran@users.noreply.github.com>
2019-10-02 15:40:01 -0400

    :sparkles: Add functioning soft and hard interruption incrementers

---
+++ PearTime/SetupViewController.h
@@ -0,0 +1,17 @@
+//
+//  SetupViewController.h
+//  PearTime
+//
+//  Created by jamie tran on 2019-10-02.
+//  Copyright © 2019 Mateus Gurgel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SetupViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PearTime/SetupViewController.m
@@ -0,0 +1,32 @@
+//
+//  SetupViewController.m
+//  PearTime
+//
+//  Created by jamie tran on 2019-10-02.
+//  Copyright © 2019 Mateus Gurgel. All rights reserved.
+//
+
+#import "SetupViewController.h"
+
+@interface SetupViewController ()
+
+@end
+
+@implementation SetupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
