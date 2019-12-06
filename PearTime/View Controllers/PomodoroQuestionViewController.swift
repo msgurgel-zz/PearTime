239fb5b28c7fd0c72ebc80d8078aaf3d5b6102ca
JamieTran <32204301+JamieTran@users.noreply.github.com>
2019-12-06 17:22:44 -0500

    :sparkles: Add core data

--- PearTime/View Controllers/PomodoroQuestionViewController.swift
+++ PearTime/View Controllers/PomodoroQuestionViewController.swift
@@ -7,25 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
 class PomodoroQuestionViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addQuestionTextField: UITextField!
     
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     var questions: [String] = [NSLocalizedString("ReflectionQuestion_1", comment: ""),
                                NSLocalizedString("ReflectionQuestion_2", comment: ""),
                                NSLocalizedString("ReflectionQuestion_3", comment: "")]
     
+    var questionss: [QuestionDB] = []
+    
+    var locations  = [QuestionDB]()
+
+    
     
     @IBAction func addButtonTapped(_ sender: Any) {
-        insertNewQuestion()
+        insertNewQuestion(aQuestion: addQuestionTextField.text!)
     }
     
     // insert new row
-    func insertNewQuestion(){
-        questions.append(addQuestionTextField.text!)
+    func insertNewQuestion(aQuestion: String){
+        questions.append(aQuestion)
         
         let indexPath = IndexPath(row: questions.count-1, section: 0)
+        let entity = NSEntityDescription.entity(forEntityName: "QuestionDB", in: context)
+        let newQuestion = NSManagedObject(entity: entity!, insertInto: context)
+        newQuestion.setValue(aQuestion, forKey: "desc")
         
         tableView.beginUpdates()
         tableView.insertRows(at: [indexPath], with: .automatic)
@@ -37,12 +49,25 @@ class PomodoroQuestionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "QuestionDB")
+        do {
+            try
+                locations = context.fetch(fetchRequest) as! [QuestionDB]
+                print("Loading DB")
+            
+            for location in locations {
+                questions.append(location.desc!)
+                try context.save()
+            }
+        } catch {
+            print("Could not fetch/save data")
+        }
 
         tableView.delegate = self
         tableView.dataSource = self
         tableView.tableFooterView = UIView(frame: CGRect.zero)
     }
-
 }
 
 extension PomodoroQuestionViewController: UITableViewDataSource, UITableViewDelegate{
@@ -70,7 +95,18 @@ extension PomodoroQuestionViewController: UITableViewDataSource, UITableViewDele
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete{
-            questions.remove(at: indexPath.row)
+            let removedQuestion = questions.remove(at: indexPath.row)
+            for location in locations{
+                if location.desc == removedQuestion{
+                    context.delete(location)
+                    do {
+                        try context.save()
+                    } catch{
+                        print("Could not save")
+                    }
+                    
+                }
+            }
             
             tableView.beginUpdates()
             tableView.deleteRows(at: [indexPath], with: .automatic)
